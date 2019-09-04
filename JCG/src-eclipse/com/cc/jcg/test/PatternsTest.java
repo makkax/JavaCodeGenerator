12ca1acf41700791d9c900f309d6871e53c271dd
Alec Bigger <alec.bigger@gmail.com>
2019-09-04 10:52:56 +0200

    tests

--- JCG/src-eclipse/com/cc/jcg/test/PatternsTest.java
+++ JCG/src-eclipse/com/cc/jcg/test/PatternsTest.java
@@ -7,6 +7,9 @@ import org.junit.Test;
 import com.cc.jcg.MBundle;
 import com.cc.jcg.MPackage;
 import com.cc.jcg.MParameter;
+import com.cc.jcg.proxy.Example;
+import com.cc.jcg.proxy.MGenerateDelegator;
+import com.cc.jcg.proxy.MGenerateMethodsProxy;
 
 public class PatternsTest {
 
@@ -15,6 +18,24 @@ public class PatternsTest {
 	MBundle bundle = new MBundle(new File("src-generated"), "Patterns Test Bundle");
 	MPackage pckg = bundle.newPackage("com.cc.jcg.examples");
 	pckg.newBean("TestBean", new MParameter(String.class, "string"), new MParameter(Integer.class, "number"));
+	bundle.generateCode(false);
+    }
+
+    @Test
+    public void test2() throws Exception {
+	MBundle bundle = new MBundle(new File("src-generated"));
+	MPackage pckg = bundle.newPackage("com.cc.jcg.examples3");
+	MGenerateDelegator generator = new MGenerateDelegator(pckg, Example.class);
+	generator.generate("TestExampleProxy");
+	bundle.generateCode(false);
+    }
+
+    @Test
+    public void test3() throws Exception {
+	MBundle bundle = new MBundle(new File("src-generated"));
+	MPackage pckg = bundle.newPackage("com.cc.jcg.examples3");
+	MGenerateMethodsProxy generator = new MGenerateMethodsProxy(pckg, Example.class);
+	generator.generate("TestExampleMethodsProxy");
 	bundle.generateCode(false);
     }
 }
