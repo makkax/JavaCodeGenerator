e63fcc2b18e55c682f6d640663d4d966dcacbd6b
Alec Bigger <alec.bigger@gmail.com>
2019-09-04 11:07:48 +0200

    simplify

--- JCG/src/com/cc/jcg/hierarchy/Hierarchy.java
+++
@@ -1,8 +0,0 @@
-package com.cc.jcg.hierarchy;
-
-public interface Hierarchy<NI> {
-
-    ParentNode<NI> getRootNode();
-
-    void visit(HierarchyVisitor<NI> visitor);
-}
--- JCG/src/com/cc/jcg/hierarchy/HierarchyVisitor.java
+++
@@ -1,6 +0,0 @@
-package com.cc.jcg.hierarchy;
-
-public interface HierarchyVisitor<NI> {
-
-    void visit(ParentNode<NI> item);
-}
