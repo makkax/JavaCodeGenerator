4f9663447945abf7e2f91b10ac70ea69ee87476e
Alec Bigger <alecbigger@users.noreply.github.com>
2019-09-05 11:24:31 +0200

    1.1

--- release.cmd
+++ release.cmd
@@ -1,1 +1,1 @@
-mvn install:install-file -DgroupId=cc -DartifactId=jcg -Dversion=1.0 -Dfile=..\JavaCodeGenerator\JCG\target\jcg-1.0.jar -Dpackaging=jar -DpomFile=..\JavaCodeGenerator\JCG\pom.xml -DlocalRepositoryPath=. -DcreateChecksum=true
+mvn install:install-file -DgroupId=cc -DartifactId=jcg -Dversion=1.1 -Dfile=..\JavaCodeGenerator\JCG\target\jcg-1.1.jar -Dpackaging=jar -DpomFile=..\JavaCodeGenerator\JCG\pom.xml -DlocalRepositoryPath=. -DcreateChecksum=true
